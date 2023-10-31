029cdc5bf51c3c1dda4441e10e6736b051f2e865
LatypovAlmaz <146587126+LatypovAlmaz@users.noreply.github.com>
2023-10-31 23:52:21 +0500

    Обновил файл Фт-220007 - Латыпов.py
    Исправил ошибки с расшифровкой

--- Фт-220007 - Латыпов.py
+++ Фт-220007 - Латыпов.py
@@ -1,4 +1,5 @@
 alpha = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz0123456789,.!?;:\'\"-/()[]'
+alpha1 = '][)(/-"\'\:;?!.,9876543210zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcba'
 choice = int(input('Выберите режим работы 1 - зашифровать, 2 - расшифровать: '))
 def match(text, alphabet=set('abcdefghijklmnopqrstuvwxyz')):
     return not alphabet.isdisjoint(text.lower())
@@ -34,26 +35,32 @@ if choice == 1:
     print(textf)
 elif choice == 2:
     text = input('Введите строку для расшифровки: ')
+    while match(text) is False:
+        print('Ошибка!!! Попробуйте снова')
+        text = input('Введите строку для расшифровки: ')
     step = int(input('Введите шаг сдвига (не больше 26): '))
+    while step > 26:
+        print('Ошибка!!! Попробуйте заново')
+        step = int(input('Введите шаг сдвига (не больше 26): '))
     textf = ''
     for i in text:
-        pos = alpha.find(i.lower())  # Приведение символа к нижнему регистру
-        npos = pos - step
+        pos = alpha1.find(i.lower())  # Приведение символа к нижнему регистру
+        npos = pos + step
         if i.isupper():  # Проверка, является ли символ заглавным
             if pos <= 51:
-                if i.lower() in alpha:
-                    textf = textf + alpha[npos].upper()  # Заглавная буква остается заглавной после шифрования
+                if i.lower() in alpha1:
+                    textf = textf + alpha1[npos].upper()  # Заглавная буква остается заглавной после шифрования
                 else:
                     textf = textf + i
             else:
-                textf = textf - alpha[
+                textf = textf - alpha1[
                     pos].upper()  # Заглавная буква остается заглавной, если символ не находится в алфавите
         else:
             if pos <= 51:
-                if i.lower() in alpha:
-                    textf = textf + alpha[npos]
+                if i.lower() in alpha1:
+                    textf = textf + alpha1[npos]
                 else:
                     textf = textf + i
             else:
-                textf = textf + alpha[pos]
+                textf = textf + alpha1[pos]
     print(textf)
